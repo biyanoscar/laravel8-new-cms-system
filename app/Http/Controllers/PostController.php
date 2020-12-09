18dd48e8c92b5699c01333df09cbc2b902ad1370
biyanoscar <biyanoscar@gmail.com>
2020-12-09 09:47:52 +0700

    Creating a post from admin

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,25 @@ class PostController extends Controller
     {
         return view('blog-post', ['post' => $post]);
     }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store()
+    {
+        $inputs = request()->validate([
+            'title' => 'required|min:8|max:255',
+            'post_image' => 'file',
+            'body' => 'required'
+        ]);
+
+        if (request('post_image')) {
+            $inputs['post_image'] = request('post_image')->store('images');
+        }
+
+        auth()->user()->posts()->create($inputs);
+        return back();
+    }
 }
